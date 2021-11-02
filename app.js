b575acb480740f0c691012282225b14560712d6c
Suresh Kumawat <bskumawat3132@gmail.com>
2021-11-02 21:33:04 +0530

    Fixed CORS

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
+	require("dotenv").config();
 }
 
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const Cors = require('cors');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const Cors = require("cors");
 
-const { connectDB } = require('./dbconnect');
-const authRoutes = require('./routes/auth');
-const productRoutes = require('./routes/products');
-const userRoutes = require('./routes/users');
-const reviewRoutes = require('./routes/reviews');
-const orderRoutes = require('./routes/orders');
-const stripeRoutes = require('./routes/stripe');
-const AppError = require('./utils/appError');
-const errorHandler = require('./controllers/errors');
+const { connectDB } = require("./dbconnect");
+const authRoutes = require("./routes/auth");
+const productRoutes = require("./routes/products");
+const userRoutes = require("./routes/users");
+const reviewRoutes = require("./routes/reviews");
+const orderRoutes = require("./routes/orders");
+const stripeRoutes = require("./routes/stripe");
+const AppError = require("./utils/appError");
+const errorHandler = require("./controllers/errors");
 
 const app = express();
 
@@ -23,29 +23,29 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(methodOverride('_method'));
 
 /* Database config */
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/fernitor';
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/fernitor";
 connectDB(dbUrl);
 
 /* middlewares */
 app.use(express.json());
 app.use(cookieParser());
-app.use(Cors());
+app.use(Cors({ Credential: true }));
 
-app.use('/api/auth', authRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/reviews', reviewRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/checkout', stripeRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/checkout", stripeRoutes);
 
 /* API endpoints */
-app.get('/api', (req, res) => {
-    res.send('<h2> welcome to fernitor api </h2>');
+app.get("/api", (req, res) => {
+	res.send("<h2> welcome to fernitor api </h2>");
 });
 
-app.all('*', (req, res, next) => {
-    const err = new AppError("page not found", 404);
-    next(err);
+app.all("*", (req, res, next) => {
+	const err = new AppError("page not found", 404);
+	next(err);
 });
 
 // custom error handler
@@ -53,5 +53,5 @@ app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Serving on port ${port}`);
+	console.log(`Serving on port ${port}`);
 });
