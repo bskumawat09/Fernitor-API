10c28298c4d38c853329a01da13423b9ad8c810a
Suresh Kumawat <bskumawat3132@gmail.com>
2021-11-02 21:42:39 +0530

    Fixed CORS issue

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ connectDB(dbUrl);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(Cors({ Credential: true, origin: "https://fernitor.netlify.app" }));
+app.use(Cors({ credentials: true, origin: "https://fernitor.netlify.app" }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
